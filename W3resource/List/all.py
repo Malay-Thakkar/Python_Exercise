d9d766cbb7aaff58e580a8f8e2201d2a7bdddc18
Malay-Thakkar <its.malaythakkar@gmail.com>
2024-02-07 17:54:37 +0530

    7 feb

--- W3resource/List/all.py
+++ W3resource/List/all.py
@@ -1,7 +1,7 @@
-my_list = ["hello","Malay","1001","hmhmhmh"]
-count = 0
-for i in my_list:
-    if i[0] == i[-1]:
-        print(i, end=" ")
-        count+=1
-print(count)    
+# my_list = ["hello","Malay","1001","hmhmhmh"]
+# count = 0
+# for i in my_list:
+#     if i[0] == i[-1]:
+#         print(i, end=" ")
+#         count+=1
+# print(count)    
