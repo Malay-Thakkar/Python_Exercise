f3fb27f0675881fb2b19e4ef28d6eb451f7ec677
malay-thakkar-drcsystem <malay.thakkar@drcsystems.com>
2024-03-19 17:35:00 +0530

    19 march

--- E_commerce/E_commerce/cart/cart.py
+++ E_commerce/E_commerce/cart/cart.py
@@ -1,11 +1,28 @@
+from api.models import ProductModel
 class Cart():
-    def __init__(self,request):
+    def __init__(self, request):
         self.session = request.session
-        #get current session key
+        # get current session key
         cart = self.session.get('session_key')
-        #if no session key create new new user
+        # if no session key, create new user
         if 'session_key' not in request.session:
-            
             cart = self.session['session_key'] = {}
-            
-            self.cart = cart
+        # Assign cart to self.cart
+        self.cart = cart
+
+    def add(self, product):
+        product_id = str(product.product_id)
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+        self.session.modified = True
+
+    def __len__(self):
+        return len(self.cart)
+    
+    def get_cart_product(self):
+        cart_product_ids =list(self.cart.keys())
+        print(cart_product_ids)
+        products = ProductModel.objects.filter(product_id__in = cart_product_ids)
+        return products
--- E_commerce/E_commerce/cart/views.py
+++ E_commerce/E_commerce/cart/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from api.models import ProductModel
+from django.http import JsonResponse
+from .cart import Cart
+
 
 # Create your views here.
 def cart_summary(request):
-    return render(request,'cart.html')
+    cart=Cart(request)
+    cart_products =cart.get_cart_product
+    return render(request,'cart.html',{'cart_products':cart_products})
 
 def cart_add(request):
-    return render(request,'cart.html')
+    cart = Cart(request)
+    if request.POST.get('action')=='post':
+        product_id = int(request.POST.get('product_id'))
+        # product_id = request.POST.get('product_id')
+        #get from DB
+        product = get_object_or_404(ProductModel,product_id=product_id)
+        cart.add(product=product)
+        cart_quantity = len(cart)
+        # cart_quantity = cart.__len__()
+        response =JsonResponse({'cart_quantity':cart_quantity})
+        print("\n\n\n\n\n\n",response)
+        return response
 
 def cart_delete(request):
     return render(request,'cart.html')
