90dfef4e9320f201d4d07c069e2cd8e5e322d066
malay-thakkar-drcsystem <malay.thakkar@drcsystems.com>
2024-03-22 17:36:34 +0530

    22march

--- E_commerce/E_commerce/customer/urls.py
+++ E_commerce/E_commerce/customer/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import signin,signup,signout,resetpasswd,home,tandc,product,productdetail,profile,notfound,aboutus,contactus,thankyou,productfilter
+from .views import signin,signup,signout,forgotpasswd,home,tandc,product,productdetail,profile,notfound,aboutus,contactus,thankyou,productfilter,updateuser,changepasswd,deleteuser
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -7,15 +7,18 @@ urlpatterns = [
     path('signin/',signin,name="signin"),
     path('signup/',signup,name="signup"),
     path('signout/',signout,name="sigout"),
-    path('forgatepasswoard/',resetpasswd,name="sigout"),
-    path('',home,name="home"),
     path('terms-and-Conditions/',tandc,name="tandc"),
+    path('profile/',profile,name="profile"),
+    path('updateuser/',updateuser,name="updateuser"),
+    path('changepasswd/',changepasswd,name="changepasswd"),
+    path('forgotpasswd/',forgotpasswd,name="forgotpasswd"),
+    path('deleteuser/',deleteuser,name="deleteuser"),
+    path('',home,name="home"),
     path('product/',product,name="product"),
     path('product/<int:productid>',productdetail,name="productdetail"),
     path('productfilter/<str:categoryid>',productfilter,name="productfilter"),
-    path('profile/',profile,name="profile"),
-    path('aboutus/',aboutus,name="profile"),
-    path('contactus/',contactus,name="profile"),
+    path('aboutus/',aboutus,name="aboutus"),
+    path('contactus/',contactus,name="contactus"),
     path('404/',notfound,name="notfound"),
     path('thankyou/',thankyou,name="thankyou"),
 ]
--- E_commerce/E_commerce/customer/views.py
+++ E_commerce/E_commerce/customer/views.py
@@ -23,7 +23,6 @@ def signup(request):
 
         if not first_name or not last_name or not username or not email or not password or not phone or not Address:
             messages.error(request, "All fields are required!")
-            print(first_name ,last_name,username,email,password,phone,Address)
             return redirect('/signup/')
         
         user = User.objects.filter(username=username)
@@ -67,9 +66,6 @@ def signin(request):
 def signout(request):
     logout(request)
     return redirect('/signin')
-
-def resetpasswd(request):
-    pass
 
 def home(request):
     return render(request,"index.html")
@@ -121,5 +117,69 @@ def profile(request):
     }
     return render(request, 'profile.html', context)
 
+@login_required(login_url='/signin')
+def updateuser(request):
+    logged_in_user = request.user
+    myuser = CustomUser.objects.get(username=logged_in_user)
+    context = {
+        'myuser': myuser
+    }
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        Address = request.POST.get('Address')
 
+        if not first_name or not last_name or not username or not email or not phone or not Address:
+            messages.error(request, "All fields are required!")
+            return redirect('/profile/')
+        
+        #check username is exits or not?
+        if username != myuser.username and CustomUser.objects.filter(username=username).exists():
+                messages.error(request, "Username already taken!")
+                return redirect('/updateuser/')
+            
+        # Update user object with new values
+        myuser.first_name = first_name
+        myuser.last_name = last_name
+        myuser.username = username
+        myuser.email = email
+        myuser.phone = phone
+        myuser.Address = Address
+		
+        myuser.save()
 
+        messages.info(request, "Profile Updated Successfully!")
+        return redirect('/profile/')  
+
+    return render(request, 'updateuser.html', context)
+
+@login_required(login_url='/signin')
+def changepasswd(request):
+    logged_in_user = request.user
+    myuser = CustomUser.objects.get(username=logged_in_user)
+    if request.method == 'POST':
+        oldpasswd = request.POST.get('oldpasswd')
+        newpasswd = request.POST.get('newpasswd')
+        conformpasswd = request.POST.get('conformpasswd')
+        
+        if conformpasswd == newpasswd:
+            print(newpasswd,oldpasswd,conformpasswd)
+            print(newpasswd,myuser.password)
+            if newpasswd == myuser.password:
+                print(newpasswd,oldpasswd,conformpasswd)
+    # myuser.set_password(passwd)
+    # myuser.save()
+    return render(request,"updatepasswd.html")
+
+def forgotpasswd(request):
+    pass
+
+@login_required(login_url='/signin')
+def deleteuser(request):
+    logged_in_user = request.user
+    myuser = CustomUser.objects.get(username=logged_in_user)
+    myuser.delete()
+    return redirect('/signup/')
