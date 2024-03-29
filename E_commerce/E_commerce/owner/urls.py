128f43edc994c6162047f3fe864c8a5c8970df10
malay-thakkar-drcsystem <malay.thakkar@drcsystems.com>
2024-03-29 16:33:42 +0530

    29march

--- E_commerce/E_commerce/owner/urls.py
+++ E_commerce/E_commerce/owner/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import dashboard,admincategory,adminorder,adminproducts,adminuser
+from .views import dashboard,admincategory,adminorder,adminproducts,adminuser,adminproductsdelete,adminproductsupdate
 
 urlpatterns = [
     path('',dashboard,name="dashboard"),
     path('order/',adminorder,name="adminorder"),
     path('products/',adminproducts,name="adminproducts"),
+    path('productsupdate/<int:product_id>/', adminproductsupdate, name="adminproductsupdate"),
+    path('productsdelete/<int:product_id>/', adminproductsdelete, name="adminproductsdelete"),
     path('category/',admincategory,name="admincategory"),
     path('users/',adminuser,name="adminuser"),
 ]
