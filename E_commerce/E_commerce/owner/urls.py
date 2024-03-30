a8a4cc5bd32345b738868b0cf2327c5cea0331d4
Malay-Thakkar <its.malaythakkar@gmail.com>
2024-03-30 23:46:14 +0530

    complete custom admin category and product

--- E_commerce/E_commerce/owner/urls.py
+++ E_commerce/E_commerce/owner/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from .views import dashboard,admincategory,adminorder,adminproducts,adminuser,adminproductsdelete,adminproductsupdate
+from .views import dashboard,admincategory,adminorder,adminproducts,adminuser,adminproductsdelete,adminproductsupdate,adminproductadd,admincategoryadd,admincategorydelete,admincategoryupdate
 
 urlpatterns = [
     path('',dashboard,name="dashboard"),
     path('order/',adminorder,name="adminorder"),
     path('products/',adminproducts,name="adminproducts"),
+    path('adminproductadd/',adminproductadd,name="adminproductadd"),
     path('productsupdate/<int:product_id>/', adminproductsupdate, name="adminproductsupdate"),
     path('productsdelete/<int:product_id>/', adminproductsdelete, name="adminproductsdelete"),
     path('category/',admincategory,name="admincategory"),
+    path('categoryadd/',admincategoryadd,name="admincategoryadd"),
+    path('categoryupdate/<int:category_id>/', admincategoryupdate, name="admincategoryupdate"),
+    path('categorydelete/<int:category_id>/', admincategorydelete, name="admincategorydelete"),
     path('users/',adminuser,name="adminuser"),
 ]
--- E_commerce/E_commerce/owner/views.py
+++ E_commerce/E_commerce/owner/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from customer.models import CustomUser
@@ -55,57 +55,138 @@ def adminorder(request):
                 return render(request, '404.html', {'error': response.text})
     return redirect('/')
 
-def adminproducts(request):
+def adminuser(request):
     if request.user.is_staff:
-        domain = request.get_host()
-        products = ProductModel.objects.all()
-        return render(request,"adminproduts.html",{'products':products,'domain':domain})
-    return redirect('/')
+        return render(request,"adminuser.html")
+    return redirect('/')    
 
 def admincategory(request):
     if request.user.is_staff:
         categorys = CategoryModel.objects.all()
-        return render(request,"admincategory.html",{'categorys':categorys})
+        return render(request,"admincategory.html",{'Categorys':categorys})
     return redirect('/')
 
-def adminuser(request):
+def admincategoryadd(request):
     if request.user.is_staff:
-        return render(request,"adminuser.html")
-    return redirect('/')
-
-def adminproductsupdate(request,product_id):
-    if request.user.is_staff:
-        prod = ProductModel.objects.get(product_id=product_id)
         if request.method == 'POST':
-                product = ProductModel.objects.get(product_id=product_id)
-                if product:
-                    name = request.POST.get('name')
-                    price = request.POST.get('price')
-                    unit = request.POST.get('unit')
-                    stock = request.POST.get('stock')
-                    img = request.FILES.get('image')
-                    desc = request.POST.get('desc')
-                    category = request.POST.get('category')
-                    
-                    if not name or not price or not unit or not stock or not img or not desc or not category:
-                        messages.error(request, "All fields are required!")
-                        return redirect('/admin/products/')
-                
-                    product.name =name
-                    product.price =price
-                    product.unit =unit
-                    product.stock =stock
-                    product.img =img
-                    product.desc =desc
-                    product.category =category
-                    product.save()
-                    
-                    return redirect('/admin/products/')
-                else:
-                    return render(request,"404.html",{'error':"product id not found"})
-        else: 
-            return render(request,"updateproduct.html",{'prod':prod})
+            category = request.POST.get('category')
+            if CategoryModel.objects.filter(category=category).exists():
+                messages.error(request, f"A {category}' already exists!")
+                return redirect('/admin/products/')
+            if not category:
+                    messages.error(request,"All fields are required!")
+            newcategory = CategoryModel(
+                category=category
+            )    
+            newcategory.save()
+            messages.success(request,"category added successfully!!!")
+            return redirect('/admin/category/')
     return redirect('/')
+
+def admincategoryupdate(request, category_id):
+    if request.user.is_staff:
+        if request.method == 'POST':
+            category_obj = CategoryModel.objects.get(category_id=category_id)
+            if category_obj:
+                new_category_name = request.POST.get('category')
+                if not new_category_name:
+                    messages.error(request, "All fields are required!")
+                    return redirect('/admin/category/')
+                else:
+                    category_obj.category = new_category_name
+                    category_obj.save()
+                    messages.success(request, "Category updated successfully!!!")
+                    return redirect('/admin/category/')
+            else:
+                return render(request, "404.html", {'error': "Category not found"})
+    return redirect('/')
+
+def admincategorydelete(request,category_id):
+    if request.user.is_staff:
+        if request.method == 'POST':
+            category = CategoryModel.objects.get(category_id=category_id)
+            if category:
+                category.delete()
+                messages.error(request,"category deleted successfully!!!")
+                return redirect('/admin/category/')
+            else:
+                return render(request,"404.html",{'error':"category not found"})
+    return redirect('/')
+    
+def adminproducts(request):
+    if request.user.is_staff:
+        domain = request.get_host()
+        products = ProductModel.objects.all()
+        categorys = CategoryModel.objects.all()
+        return render(request,"adminproduts.html",{'products':products,'categorys':categorys,'domain':domain})
+    return redirect('/')
+
+def adminproductadd(request):
+    if request.user.is_staff:
+        if request.method == 'POST':
+            name = request.POST.get('name')
+            price = request.POST.get('price')
+            unit = request.POST.get('unit')
+            stock = request.POST.get('stock')
+            img = request.FILES.get('img')
+            desc = request.POST.get('desc')
+            category_id = request.POST.get('category')
+            
+            # Move the print statement after the category variable is assigned
+            category = get_object_or_404(CategoryModel, pk=category_id)
+             
+            # Check if a product with the same name already exists
+            if ProductModel.objects.filter(name=name).exists():
+                messages.error(request, f"A product with the name '{name}' already exists!")
+                return redirect('/admin/products/')
+            
+            if not name or not price or not unit or not stock or not img or not desc or not category:
+                messages.error(request, "All fields are required!")
+                return redirect('/admin/products/')
+            
+            newproduct = ProductModel(
+                name = name,price = price,unit = unit,stock = stock,img = img,desc = desc,category = category
+            )
+            newproduct.save()
+            messages.success(request,"product added successfully!!!")
+            return redirect('/admin/products/')
+        return redirect('/')
+
+def adminproductsupdate(request, product_id):
+    if request.user.is_staff:
+        if request.method == 'POST':
+            product = ProductModel.objects.get(product_id=product_id)
+            if product:
+                name = request.POST.get('name')
+                price = request.POST.get('price')
+                unit = request.POST.get('unit')
+                stock = request.POST.get('stock')
+                img = request.FILES.get('img')
+                desc = request.POST.get('desc')
+                category_id = request.POST.get('category')
+                
+                # Move the print statement after the category variable is assigned
+                category = get_object_or_404(CategoryModel, pk=category_id)
+
+                if not name or not price or not unit or not stock or not img or not desc or not category:
+                    messages.error(request, "All fields are required!")
+                    return redirect('/admin/products/')
+                
+                product.name = name
+                product.price = price
+                product.unit = unit
+                product.stock = stock
+                product.img = img
+                product.desc = desc
+                product.category = category
+                product.save()
+                messages.success(request,"product updated successfully!!!")
+                return redirect('/admin/products/')
+            else:
+                messages.error(request,"product not found")
+                return render(request, "404.html", {'error': "Product ID not found"})
+    return redirect('/')
+
     
 def adminproductsdelete(request, product_id):
     if request.user.is_staff:
@@ -113,6 +194,7 @@ def adminproductsdelete(request, product_id):
             product = ProductModel.objects.get(product_id=product_id)
             if product:
                 product.delete()
+                messages.error(request,"product deleted successfully!!!")
                 return redirect('/admin/products/')
             else:
                 return render(request,"404.html",{'error':"product id not found"})
