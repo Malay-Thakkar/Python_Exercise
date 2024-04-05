243b11646fb8c227e913b246e37595641645291b
malay-thakkar-drcsystem <malay.thakkar@drcsystems.com>
2024-04-05 10:12:12 +0530

    debug model dependancy error

--- E_commerce/E_commerce/customer/models.py
+++ E_commerce/E_commerce/customer/models.py
@@ -8,7 +8,7 @@ class CustomUser(AbstractUser):
     tandc = models.CharField(blank=True)
     old_cart = models.CharField(max_length=250,blank=True)
     old_wishlist = models.CharField(max_length=250,blank=True)
-    shipping_address = models.ForeignKey(ShippingAddressModel,on_delete=models.SET_NULL,null=True,blank=True)
+    shipping_address_model = models.ForeignKey(ShippingAddressModel,on_delete=models.SET_NULL,null=True,blank=True)
 
     # Add any other custom fields you need
     def __str__(self):
